822044636d5eabff0ba070a500f1f1436da3ff01
anttonalberdi <Ss7679Bd>
2021-06-21 14:57:40 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,40 +1,11 @@
-"""
-Build autoASV
-"""
+from setuptools import setup
 
-import sys
-import os.path
-from setuptools import setup, Extension, find_packages
-
-import pathlib
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(
-    name="autoasv",
-    version="1.0",
-    author="Antton Alberdi",
-    author_email="anttonalberdi@gmail.com",
-    description="Automatic amplicon sequencing data processing pipeline",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    scripts=['bin/autoasv'],
-    url="https://github.com/anttonalberdi/autoasv",
-    project_urls={
-        "Bug Tracker": "https://github.com/anttonalberdi/autoasv/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'setuptools',
-        'pandas >= 0.22.0',
-        'numpy >= 1.16.0'
-    ],
-    python_requires=">=3.8",
-)
+setup(name='autoasv',
+      version='1.0',
+      description='The funniest joke in the world',
+      url='https://github.com/anttonalberdi/autoasv',
+      author="Antton Alberdi",
+      author_email="anttonalberdi@gmail.com",
+      license='MIT',
+      packages=['autoasv'],
+      zip_safe=False)
